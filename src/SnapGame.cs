8f9fd5f2ff3f4b2a73e0cc5fe7fcf99306ebf99f
0xvader <alexhaider55@hotmail.com>
2019-03-25 00:52:06 +0800

    Added Chunkfive and changed font
    Added Chunkfive OTF and the text for the score is drawn using Chunkfive to make it look more interesting.

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -10,9 +10,9 @@ namespace CardGames
         {
             Bitmap cards;
             cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
-            SwinGame.BitmapSetCellDetails (cards, 82, 110, 13, 5, 53);      // set the cells in the bitmap to match the cards
+            SwinGame.BitmapSetCellDetails (cards, 82, 110, 13, 5, 53);   // set the cells in the bitmap to match the cards
+			SwinGame.LoadFontNamed ("GameFont","Chunkfive.otf", 24);
         }
-
 		/// <summary>
 		/// Respond to the user input -- with requests affecting myGame
 		/// </summary>
@@ -38,7 +38,6 @@ namespace CardGames
 				}
 			}
 		}
-
 		/// <summary>
 		/// Draws the game to the Window.
 		/// </summary>
@@ -52,14 +51,14 @@ namespace CardGames
 			Card top = myGame.TopCard;
 			if (top != null)
 			{
-				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
+				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.White, "GameFont",0, 20);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0),Color.White, "GameFont",0, 30);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1),Color.White, "GameFont",0, 40);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 350, 50);
 			}
 			else
 			{
-				SwinGame.DrawText ("No card played yet...", Color.RoyalBlue, 0, 20);
+				SwinGame.DrawText ("No card played yet...", Color.White, "GameFont",0, 20);
 			}
 
 			// Draw the back of the cards... to represent the deck
